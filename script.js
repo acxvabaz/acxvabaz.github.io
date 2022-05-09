10b24b7481693370c5b98bc5f30e852c710f3ffa
acxvabaz <80430007+acxvabaz@users.noreply.github.com>
2022-05-09 13:10:14 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+const swup = new Swup();
+
+const button = document.getElementsByClassName('button')[0]
+const links = document.getElementsByClassName('tab-but-container')[0]
+
+button.addEventListener('click', () => {
+    links.classList.toggle('clicked')
+})
+
+links.addEventListener('click', () => {
+    links.classList.toggle('clicked')
+})
